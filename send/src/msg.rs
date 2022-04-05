e15825c043ceac46525e2d838cb7dd5ef96b2077
0x7183 <daniele.demenna@gmail.com>
2022-04-05 13:17:39 -0400

    Adding send example

--- send/src/msg.rs
+++ send/src/msg.rs
@@ -1,27 +1,28 @@
+use cosmwasm_std::{Addr, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    Send {to_address: Addr},
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    // GetTransaction returns the transaction with id
+    GetTransaction {address: String, id: String},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct TransactionResponse {
+    pub sender: Addr,
+    pub receiver: Addr,
+    pub amount: Uint128,
 }
--- send/src/state.rs
+++ send/src/state.rs
@@ -1,13 +1,22 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Uint128};
+use cw_storage_plus::{Map, Item};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+
+pub struct Config{
+    pub id: i32,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct History {
+    pub id: String,
+    pub sender: Addr,
+    pub receiver: Addr,
+    pub amount: Uint128,
+}
+
+pub const HISTORY: Map<(&Addr, &str),History> = Map::new("history");
+pub const CONFIG: Item<Config> = Item::new("config");
